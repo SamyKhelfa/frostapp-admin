419d12f78f6a2b842fdefe88b585783704ba4d36
SamyKhelfa <samy.khelfa94@gmail.com>
2026-03-10 18:04:12 +0100

    community

--- src/ui/pages/Community.tsx
+++ src/ui/pages/Community.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import AdminLayout from '../components/AdminLayout/AdminLayout';
-import Card from 'antd/es/card/Card';
-import { List } from 'antd';
+import React from "react";
+import AdminLayout from "../components/AdminLayout/AdminLayout";
+import Card from "antd/es/card/Card";
+import { List, Input } from "antd";
 
 export const Community: React.FC = () => {
-    return (
-        <AdminLayout>
-            <Card>
+  return (
+    <AdminLayout>
+      <Card>
         <div className="community-page">
-            <h1>Community</h1>
-            <p>Welcome to the Community page</p>
+          <h1>Community</h1>
+          <Input placeholder="Rechercher un utilisateur" />
+          <p>Utilisateurs inscrits récemment</p>
+          <List
+            itemLayout="horizontal"
+            dataSource={[]}
+            renderItem={(item) => (
+              <List.Item>
+                <List.Item.Meta title={item.name} description={item.email} />
+              </List.Item>
+            )}
+          />
         </div>
-        </Card>
-        </AdminLayout>
-    );
+      </Card>
+    </AdminLayout>
+  );
 };
 
 export default Community;
